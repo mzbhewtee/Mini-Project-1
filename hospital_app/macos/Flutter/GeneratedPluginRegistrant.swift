377882b33b0f1495b2d8a56f20e47c2219be6bfa
Moyo-tech <m.weke@alustudent.com>
2023-02-14 23:36:45 +0200

    complete project

--- hospital_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ hospital_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-=======
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> af3cddcb6ce4ae02b4c68f0af11857786c3f0fc5
 }
