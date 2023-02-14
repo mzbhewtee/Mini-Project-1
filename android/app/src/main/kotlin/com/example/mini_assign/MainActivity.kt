6f0f76ca04e7e3899c1df7f8b851fc82ec745c2b
Kundai Chasinda <chasindak9@gmail.com>
2023-02-14 17:10:58 +0200

    Added a splash screen and a dummy home page to the repository

---
+++ android/app/src/main/kotlin/com/example/mini_assign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mini_assign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// this is just a dummy page so that the welcome screen runs smoothly. Please remove it and replace it with the code you wrote
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Text("Hie"));
+  }
+}
